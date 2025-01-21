2205e4e62f30effbbfb7a162107df49441c1858d
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2025-01-21 07:17:41 -0500

    Adds a missing export that prevents the Sync Status tool from being fully usable

--- lib/ditto_flutter_tools.dart
+++ lib/ditto_flutter_tools.dart
@@ -2,4 +2,4 @@
 // export "src/disk_usage/disk_usage.dart" show DiskUsage;
 // export "src/log_level_switch.dart" show LogLevelSwitch;
 export "src/sync_status_view.dart" show SyncStatusView;
-export "src/sync_status_helper.dart" show SyncStatusHelper;
+export "src/sync_status_helper.dart" show SyncStatusHelper, SyncStatus;
